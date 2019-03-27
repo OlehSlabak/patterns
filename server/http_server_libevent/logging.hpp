bf02b8e7ec21dde918ee35ba2e0b14d26554da17
OlehSlabak <pesekot@gmail.com>
2019-03-27 20:55:47 +0200

    first commit. Add http_server_libevent

---
+++ server/http_server_libevent/logging.hpp
@@ -0,0 +1,18 @@
+#ifndef LOGGING_HPP_INCLUDED
+#define LOGGING_HPP_INCLUDED
+
+/* logging singleton */
+
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <mutex>
+#include <unordered_map>
+#include <memory>
+#include <chrono>
+#include <ctime>
+#include <cstdlib>
+
+
+#endif // LOGGING_HPP_INCLUDED
---
+++ server/http_server_libevent/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <event.h>
+#include <evhttp.h>
+
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <vector>
+#include <functional>
+#include <memory>
+
+
+#include "logging.hpp"
+
+//#include "tcp_server_socket.h"
+//#include "inet_address_v4.h"
+#include <thread>
+//#include "command_line.h"
+//#include "logger.h"
+//#include "file_holder.h"
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
